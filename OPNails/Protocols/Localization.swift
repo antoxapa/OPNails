7972e54ea21eb9c7b7aef9447fdf7d66a2e37fd5
Tony Po <antoxapa@gmail.com>
2020-09-05 17:29:55 +0300

    Add localization for Eng/Rus, fix bugs

---
+++ OPNails/Protocols/Localization.swift
@@ -0,0 +1,9 @@
+//
+//  Localization.swift
+//  OPNails
+//
+//  Created by Антон Потапчик on 9/4/20.
+//  Copyright © 2020 TonyPo Production. All rights reserved.
+//
+
+import Foundation
---
+++ OPNails/SupportingFiles/Localization/LocalizationText.swift
@@ -0,0 +1,67 @@
+//
+//  localizationText.swift
+//  OPNails
+//
+//  Created by Антон Потапчик on 9/4/20.
+//  Copyright © 2020 TonyPo Production. All rights reserved.
+//
+
+import Foundation
+
+struct i18n {
+    
+    static var buttonOk: String { return "button_ok".localized }
+    static var buttonYes: String { return "button_yes".localized }
+    
+    static var errorTitle: String { return "error_title".localized }
+    static var alertLoad: String { return "alert_load".localized }
+    
+    static var userName: String { return "user_name".localized }
+    static var userMail: String { return "user_mail".localized }
+    static var userPhone: String { return "user_phone".localized }
+    static var userPassword: String { return "user_password".localized }
+    
+    static var buttonRegister: String { return "button_register".localized }
+    static var buttonCancel: String { return "button_cancel".localized }
+    static var buttonLogin: String { return "button_login".localized }
+    static var dontHaveAccount: String { return "dont_have_accaunt".localized }
+    
+    static var todayTitle: String { return "today_title".localized }
+    static var priceTitle: String { return "price_title".localized }
+    static var addTitle: String { return "add_title".localized }
+    static var buttonLogout: String { return "button_logout".localized }
+    
+    static var mondayTitle: String { return "monday_title".localized }
+    static var tuesdayTitle: String { return "tuesday_title".localized }
+    static var wednesdayTitle: String { return "wednesday_title".localized }
+    static var thursdayTitle: String { return "thursday_title".localized }
+    static var fridayTitle: String { return "friday_title".localized }
+    static var saturdayTitle: String { return "saturday_title".localized }
+    static var sundayTitle: String { return "sunday_title".localized }
+    
+    static var emptyTitle: String { return "empty_title".localized }
+    static var emptyUsers: String { return "empty_users".localized }
+    
+    static var newEntry: String { return "new_entry".localized }
+    
+    static var timeAvailable: String { return "time_available".localized }
+    static var timeYours: String { return "time_yours".localized }
+    
+    static var signUpAt: String { return "sign_up_at".localized }
+    static var entryAlreadyExist: String { return "entry_already_exist".localized }
+    
+    static var cancelEntryBefore: String { return "cancel_entry_before".localized }
+    static var cancelEntry: String { return "cancel_entry".localized }
+    static var removeClient: String { return "remove_client".localized }
+    
+    static var areYouSureRemoveClient: String { return "sure_remove_client".localized }
+    
+    static var change_title: String { return "change_title".localized }
+    static var new_title: String { return "new_title".localized }
+    
+    static var registration_title: String { return "registration_title".localized }
+    
+    static var correct_title: String { return "correct_title".localized }
+    static var selectedDays_title: String { return "selected_days_title".localized }
+    
+}
